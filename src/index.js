13ba4aa6a4dbff87ceb87f4289fc15cbf12ed853
Andrei Rushnitski <andrejrusik@gmail.com>
2022-01-13 15:08:57 +0100

    feat: add function "check"

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let open = [];
+  for (let i = 0; i < str.length; i++) {
+    loop: for (let el = 0; el < bracketsConfig.length; el++) {
+      if (bracketsConfig[el][0] == bracketsConfig[el][1]) {
+        if (open[open.length - 1] == str[i]) {
+          open.pop();
+          break loop;
+        } else {
+          open.push(str[i]);
+          break loop;
+        }
+      }
+      if (bracketsConfig[el][0] == str[i]) {
+        open.push(str[i]);
+        break loop;
+      }
+      if (bracketsConfig[el][1] == str[i]) {
+        if (open[open.length - 1] == bracketsConfig[el][0]) {
+          open.pop();
+          break loop;
+        }
+        return false;
+      }
+    }
+  }
+  return open.length == 0 ? true : false;
+};
